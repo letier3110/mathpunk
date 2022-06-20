78902f4f1b9eed403282f616d06ea70f2bd2dfb8
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-20 21:22:28 +0100

    added second class

--- spire_mvp_flutter/lib/classes/base_character.dart
+++ spire_mvp_flutter/lib/classes/base_character.dart
@@ -17,3 +17,42 @@ class ICharacter {
     maxHealth = 0;
   }
 }
+
+class BaseCharacter implements ICharacter {
+  late String name;
+  @override
+  late int health;
+  @override
+  late int maxHealth;
+
+  BaseCharacter() {
+    health = 10;
+    maxHealth = 10;
+    name = '';
+  }
+
+  @override
+  getHealth() {
+    return health;
+  }
+
+  @override
+  getMaxHealth() {
+    return maxHealth;
+  }
+
+  @override
+  setHealth(int health) {
+    this.health = health;
+  }
+
+  @override
+  changeHealth(int health) {
+    this.health -= health;
+  }
+
+  @override
+  setMaxHealth(int maxHealth) {
+    this.maxHealth = maxHealth;
+  }
+}

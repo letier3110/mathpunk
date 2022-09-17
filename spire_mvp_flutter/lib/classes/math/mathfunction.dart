d993ef93808cec27809f590b4ccfa2f62dedc19a
letier3110 <letier3110@gmail.com>
2022-09-17 22:19:06 +0100

    wip

---
+++ spire_mvp_flutter/lib/classes/math/mathfunction.dart
@@ -0,0 +1,16 @@
+import 'package:mathpunk_cardgame/classes/card/playable_card.dart';
+
+// TODO: add following structure
+// Basis => Slots => PlayableCard | Basis | undefined
+class Mathfunction {
+  List<Mathslot> slots = [];
+  Mathfunction() {}
+}
+
+class Mathslot {
+  late PlayableCard? card;
+  late Mathfunction? mathfunction;
+
+  // Moveset(Enemy enemyParent) {
+  Mathslot() {}
+}

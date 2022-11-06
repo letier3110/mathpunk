aaff5d22fa7f3bda748cff2697789f8a7171d9fa
letier3110 <letier3110@gmail.com>
2022-11-06 14:45:37 +0000

    add audio lib

--- spire_mvp_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ spire_mvp_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import path_provider_macos
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
